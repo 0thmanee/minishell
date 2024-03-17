a3dd295b6e0c3a0e339f02100a6f8876483947e0
03yasser <yasserboutslighoi03@gmail.com>
2024-03-17 00:54:34 +0000

    done

---
+++ builtins/env.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouchta <obouchta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 14:36:05 by yboutsli          #+#    #+#             */
+/*   Updated: 2024/03/07 19:50:49 by obouchta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	env(t_list *list_env)
+{
+	t_list	*current;
+
+	if (!list_env)
+		return (1);
+	current = list_env;
+	while (current)
+	{
+		printf("%s=%s\n", current->var, current->value);
+		current = current->next;
+	}
+	return (0);
+}
