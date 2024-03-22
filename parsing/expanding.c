d2be89815559cb52b98100131e7d27ea0645465a
bouchtaothmane <bouchtaothmane>
2024-03-22 03:09:23 +0000

    expanding

--- parsing/expanding.c
+++ parsing/expanding.c
@@ -6,49 +6,50 @@
 /*   By: obouchta <obouchta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 02:58:32 by yboutsli          #+#    #+#             */
-/*   Updated: 2024/03/21 07:35:13 by obouchta         ###   ########.fr       */
+/*   Updated: 2024/03/22 02:35:47 by obouchta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-char *remove_char(char *str, int char_index)
+
+char *remove_char(char *str, int char_index, t_free **ptrs)
 {
 	char	*tmp1;
 	char	*tmp2;
 
-	tmp1 = ft_substr(str, 0, char_index);
-	tmp2 = ft_substr(str, char_index + 1, ft_strlen(str) - char_index - 1);
-	free(str);
+	tmp1 = ft_substr(str, 0, char_index, ptrs);
+	tmp2 = ft_substr(str, char_index + 1, ft_strlen(str) - char_index - 1, ptrs);
+	ft_free_ptr(ptrs, str);
 	if (!tmp1)
 		return (tmp2);
 	else if (!tmp2)
 		return (tmp1);
-	str = ft_strjoin(tmp1, tmp2);
-	free(tmp1);
-	free(tmp2);
+	str = ft_strjoin(tmp1, tmp2, ptrs);
+	ft_free_ptr(ptrs, tmp1);
+	ft_free_ptr(ptrs, tmp2);
 	return (str);
 }
-char	*replace_mini_str(char *str, int start, int end, char *mini_str)
+char	*replace_mini_str(char *str, int start, int end, char *mini_str, t_free **ptrs)
 {
 	char	*tmp1;
 	char	*tmp2;
 	char	*tmp3;
 
-	tmp1 = ft_substr(str, 0, start);
+	tmp1 = ft_substr(str, 0, start, ptrs);
 	if (!tmp1 && mini_str)
-		tmp2 = ft_strdup_1(mini_str);
+		tmp2 = ft_strdup(mini_str, ptrs);
 	else
 	{
-		tmp2 = ft_strjoin(tmp1, mini_str);
-		free(tmp1);
+		tmp2 = ft_strjoin(tmp1, mini_str, ptrs);
+		ft_free_ptr(ptrs, tmp1);
 	}
-	tmp3 = ft_substr(str, end, ft_strlen(str) - end + 1);
+	tmp3 = ft_substr(str, end, ft_strlen(str) - end + 1, ptrs);
 	if (!tmp3)
 		return (tmp2);
 	else
-		tmp1 = ft_strjoin(tmp2, tmp3); 
-	free(tmp3);
-	free(tmp2);
+		tmp1 = ft_strjoin(tmp2, tmp3, ptrs);
+	ft_free_ptr(ptrs, tmp3);
+	ft_free_ptr(ptrs, tmp2);
 	return (tmp1);
 }
 int char_is_valid(char c)
@@ -60,7 +61,7 @@ int char_is_valid(char c)
 	else
 		return (0);
 }
-char	*case_1(char *result, int *i, t_list *list_env)
+char	*case_1(char *result, int *i, t_list *list_env, t_free **ptrs)
 {
 	int		j;
 	char	*tmp;
@@ -70,16 +71,15 @@ char	*case_1(char *result, int *i, t_list *list_env)
 	j = 1;
 	while (!char_is_valid(result[j + *i]))
 		j++;
-	var = ft_substr(result, *i + 1, j - 1);
+	var = ft_substr(result, *i + 1, j - 1, ptrs);
 	value = get_env(&list_env, var);
-	free(var);
-	var = NULL;
+	ft_free_ptr(ptrs, var);
 	if (value != NULL)
 	{
 		tmp = result;
-		result = replace_mini_str(tmp, *i,  *i + j, value);
+		result = replace_mini_str(tmp, *i,  *i + j, value, ptrs);
 		*i += ft_strlen(value);
-		free(tmp);
+		ft_free_ptr(ptrs, tmp);
 	}
 	else
 	{
@@ -87,65 +87,99 @@ char	*case_1(char *result, int *i, t_list *list_env)
 		while (!char_is_valid(result[j + *i]))
 			j++;
 		if (result[j + *i] != '\0')
-			var = ft_substr(result, j + *i, ft_strlen(result) - j - *i);
+			var = ft_substr(result, j + *i, ft_strlen(result) - j - *i, ptrs);
 		tmp = result;
-		result = ft_substr(tmp, 0, *i - 1);
-		free(tmp);
+		result = ft_substr(tmp, 0, *i - 1, ptrs);
+		ft_free_ptr(ptrs, tmp);
 		if (var)
 		{
 			tmp = result;
-			result = ft_strjoin(tmp, var);
-			free(tmp);
-			free(var);
+			result = ft_strjoin(tmp, var, ptrs);
+			ft_free_ptr(ptrs, tmp);
+			ft_free_ptr(ptrs, var);
 			(*i)--;
 		}
 	}
 	return (result);
 }
-char	*case_2(char *result, int *i)
+char	*case_2(char *result, int *i, t_free **ptrs)
 {
-	result = remove_char(result, *i);
+	result = remove_char(result, *i, ptrs);
 	return (result);
 }
 
-char	*case_3(char *result, int *i)
+char	*case_3(char *result, int *i, t_free **ptrs)
 {
-	result = remove_char(result, *i);
-	result = remove_char(result, *i);
+	result = remove_char(result, *i, ptrs);
+	result = remove_char(result, *i, ptrs);
 	(*i)++;
 	return (result);
 }
 
-char	*expanding_helper(t_list *list_env, char *value, int vars_len, int *vars)
+char	*expanding_helper1(t_list *list_env, t_token *token, t_free **ptrs)
 {
 	int	i;
 	int	count;
 	char *result;
-	i = 0;
-	count = 0;
-	
-	result = ft_strdup_1(value);
-	if (!result)
-		return (0);
-	free(value);
-	while (result[i] && count < vars_len)
+
+	(i = 0, count = 0, result = ft_strdup(token->value, ptrs));
+	while (result[i] && count < token->vars_len)
 	{
 		if (result[i] == '$' && !result[i + 1])
 			return (result);
 		if (result[i] == '$' && result[i + 1] != '\0')
 		{
-			if (vars[count] == 0)
+			if (token->vars[count] == 0)
 			{
 				i++;
 				count++;
 				continue;
 			}
-			else if (vars[count] == 1)
-				result = case_1(result, &i, list_env);
-			else if (vars[count] == 2)
-				result = case_2(result, &i);
-			else if (vars[count] == 3)
-				result = case_3(result, &i);
+			else if (token->vars[count] == 1)
+				result = case_1(result, &i, list_env, ptrs);
+			else if (token->vars[count] == 2)
+				result = case_2(result, &i, ptrs);
+			else if (token->vars[count] == 3)
+				result = case_3(result, &i, ptrs);
+			if (!result)
+				return (0);
+			count++;
+		}
+		if (i > ft_strlen(result) || !result[i])
+			break;
+		else if (result[i] == '$')
+			continue;
+		i++;
+	}
+	return (result);
+}
+
+char	*expanding_helper2(t_list *list_env, t_value args, t_free **ptrs)
+
+{
+	int	i;
+	int	count;
+	char *result;
+
+	(i = 0, count = 0, result = ft_strdup(args.value, ptrs));
+	while (result[i] && count < args.vars_len)
+	{
+		if (result[i] == '$' && !result[i + 1])
+			return (result);
+		if (result[i] == '$' && result[i + 1] != '\0')
+		{
+			if (args.vars[count] == 0)
+			{
+				i++;
+				count++;
+				continue;
+			}
+			else if (args.vars[count] == 1)
+				result = case_1(result, &i, list_env, ptrs);
+			else if (args.vars[count] == 2)
+				result = case_2(result, &i, ptrs);
+			else if (args.vars[count] == 3)
+				result = case_3(result, &i, ptrs);
 			if (!result)
 				return (0);
 			count++;
@@ -160,20 +194,26 @@ char	*expanding_helper(t_list *list_env, char *value, int vars_len, int *vars)
 	return (result);
 }
 
-void	expanding(t_token **tokens, t_list *list_env)
+void	expanding(t_token **tokens, t_list *list_env, t_free **ptrs)
 {
 	int	i;
 	t_token	*curr;
+	char	*tmp_val;
 
 	curr = *tokens;
 	while (curr)
 	{
-		curr->value = expanding_helper(list_env,
-			curr->value, curr->vars_len, curr->vars);
+		tmp_val = curr->value;
+		curr->value = expanding_helper1(list_env, curr, ptrs);
+		ft_free_ptr(ptrs, tmp_val);
 		i = -1;
 		while (++i < curr->args_len)
-			curr->args[i].value = expanding_helper(list_env, curr->args[i].value,
-				curr->args[i].vars_len, curr->args[i].vars);
+		{
+			tmp_val = curr->args[i].value;
+			curr->args[i].value = expanding_helper2(list_env, curr->args[i], ptrs);
+			ft_free_ptr(ptrs, tmp_val);
+		}
 		curr = curr->next;
 	}
 }
+
