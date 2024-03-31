9e828c0c749d1aa1f028cfe7f977397380ec9617
03yasser <yasserboutslighoi03@gmail.com>
2024-03-31 03:57:02 +0000

    exit done

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,12 +6,16 @@
 /*   By: yboutsli <yboutsli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 09:26:13 by yboutsli          #+#    #+#             */
-/*   Updated: 2024/03/30 03:00:03 by yboutsli         ###   ########.fr       */
+/*   Updated: 2024/03/31 02:28:24 by yboutsli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+int	new_perror(char *str)
+{
+	perror(str);
+	return (1);
+}
 int	last_bs(char *str)
 {
 	int	i;
@@ -23,12 +27,29 @@ int	last_bs(char *str)
 		return (0);
 	return(1);
 }
+void	cd_dir_utils(t_list **list_env, char *pwd, char *tmp[2])
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (cwd)
+	{
+		env_update(list_env, "OLDPWD", pwd);
+		env_update(list_env, "PWD", cwd);
+		free(cwd);
+	}
+	else
+	{
+		perror("getcwd: ");
+		env_update(list_env, "PWD", tmp[1]);
+		env_update(list_env, "OLDPWD", pwd);
+	}
+}
 int	cd_dir(char *str, t_list **list_env)
 {
 	char	*tmp[3];
 	int		status;
 	char	*pwd;
-	char	*cwd;
 
 	status = 0;
 	pwd = get_env(list_env, "PWD");
@@ -38,29 +59,10 @@ int	cd_dir(char *str, t_list **list_env)
 		tmp[0] = ft_strjoin_2("/", str);
 	tmp[1] = ft_strjoin_2(pwd, tmp[0]);
 	if (chdir(str) == 0)
-	{
-		cwd = getcwd(NULL, 0);
-		if (cwd)
-		{
-			env_update(list_env, "OLDPWD", pwd);
-			env_update(list_env, "PWD", cwd);
-			free(cwd);
-		}
-		else
-		{
-			perror("getcwd: ");
-			env_update(list_env, "PWD", tmp[1]);
-			env_update(list_env, "OLDPWD", pwd);
-		}
-	}
+		cd_dir_utils(list_env, pwd, tmp);
 	else
-	{
-		perror(str);
-		status = 1;
-	}
-	free(tmp[1]);
-	free(tmp[0]);
-	return (status);
+		status = new_perror(str);
+	return (free(tmp[1]), free(tmp[0]), status);
 }
 
 int	cd_root(char *str, t_list **list_env)
@@ -72,7 +74,9 @@ int	cd_root(char *str, t_list **list_env)
 	status = 0;
 	if (chdir(str) == -1)
 	{
-		printf("cd: no such file or directory: %s\n", str);
+		ft_putstr_fd("cd: no such file or directory: ", 2);
+		ft_putstr_fd(str, 2);
+		ft_putstr_fd("\n", 2);
 		status = 1;
 	}
 	else
@@ -92,7 +96,7 @@ int	cd_home(t_list **env, char *str)
 	home = get_env(env, "HOME");
 	if (!home)
 	{
-		printf("minishell: cd: HOME not set\n");//write(2)
+		ft_putstr_fd("minishell: cd: HOME not set\n", 2);
 		return (1);
 	}
 	else if (chdir(home) == -1)
---
+++ builtins/exit.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yboutsli <yboutsli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 00:17:09 by yboutsli          #+#    #+#             */
+/*   Updated: 2024/03/31 02:17:00 by yboutsli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+void	error_msg(char *str)
+{
+	ft_putstr_fd("exit\nminishell: exit: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
+	exit (255);
+}
+
+long	atoi_long(char *str)
+{
+	long long	nb;
+	int		sign;
+	int		i;
+
+	nb = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		nb = (nb * 10) + (str[i] - 48);
+		i++;
+	}
+	return (nb * sign);	
+}
+int	long_nb(char *s)
+{
+	long long	nb;
+	int		sign;
+	int		i;
+
+	nb = 0;
+	sign = 1;
+	i = 0;
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (s[i])
+	{
+		nb = (nb * 10) + (s[i] - 48);
+		if ((nb * sign) > LONG_MAX || (nb * sign) < LONG_MIN)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	valid_args(char **args)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while(args[++i])
+	{
+		if (i > 0)
+		{
+			ft_putstr_fd("exit\n", 2);
+			ft_putstr_fd("minishel: exit: too many arguments\n", 2);
+			return (1);
+		}
+		j = -1;
+		if (args[i][0] == '-' && args[i][1] != '\0')
+			j++;
+		while (args[i][++j])		
+		{
+			if (ft_isdigit(args[i][j]))
+				error_msg(args[i]);
+		}
+		if (long_nb(args[i]))
+			error_msg(args[i]);
+	}
+	return (0);
+}
+void	ft_exit_utils(char *arg)
+{
+	long nb;
+
+	nb = atoi_long(arg);
+	//ft_free
+	exit(nb);
+}
+int	ft_exit(t_cmd *cmd)
+{
+	if (!cmd->args)
+	{
+		exit (0);//free
+	}
+	else if (valid_args(cmd->args))
+		return (1);
+	else
+		ft_exit_utils(cmd->args[0]);
+	return (0);
+}
