050f633bbfdedfd801fa70a817e063b8ee3f39c0
bouchtaothmane <bouchtaothmane>
2024-04-24 22:13:45 +0100

    cd

--- parsing/signals.c
+++ parsing/signals.c
@@ -6,7 +6,7 @@
 /*   By: obouchta <obouchta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 03:27:56 by obouchta          #+#    #+#             */
-/*   Updated: 2024/04/24 21:40:12 by obouchta         ###   ########.fr       */
+/*   Updated: 2024/04/24 22:04:25 by obouchta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ void	handle_signals(int signum)
 	if (signum == SIGINT)
 	{
 		if (waitpid(-1, NULL, WNOHANG) == 0)
+		{
+			ft_putstr_fd("\n", 1);
 			return ;
+		}
 		exit_status(0, 1);
 		ft_putstr_fd("\n", 2);
 		rl_replace_line("", 0);
